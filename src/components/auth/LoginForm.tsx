a85dff90b2d5edbde5379e026a4669e20d239a28
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-07 07:44:00 +0900

    ログインフォームのスタイルを簡素化し、不要なインポートを削除しました。また、エラーメッセージの表示方法を改善しました。

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,13 +1,6 @@
 "use client";
-
-import { lusitana } from "@/app/ui/fonts";
-import {
-  AtSymbolIcon,
-  KeyIcon,
-  ExclamationCircleIcon,
-} from "@heroicons/react/24/outline";
 import { useActionState } from "react";
-import { authenticate } from "@/app/lib/actions";
+import { authenticate } from "@/lib/actions/authenticate";
 import { useSearchParams } from "next/navigation";
 
 export default function LoginForm() {
@@ -21,9 +14,7 @@ export default function LoginForm() {
   return (
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
-        </h1>
+        <h1 className="mb-3 text-2xl">Please log in to continue.</h1>
         <div className="w-full">
           <div>
             <label
@@ -41,7 +32,6 @@ export default function LoginForm() {
                 placeholder="Enter your email address"
                 required
               />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
           <div className="mt-4">
@@ -61,7 +51,6 @@ export default function LoginForm() {
                 required
                 minLength={6}
               />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
         </div>
@@ -75,10 +64,9 @@ export default function LoginForm() {
           aria-atomic="true"
         >
           {errorMessage && (
-            <>
-              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+            <div className="h-5 w-5 text-red-500">
               <p className="text-sm text-red-500">{errorMessage}</p>
-            </>
+            </div>
           )}
         </div>
       </div>
