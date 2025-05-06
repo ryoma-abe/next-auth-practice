6479d4586cafad67278a663987ca35d483722502
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-07 07:41:57 +0900

    認証レイアウトを更新し、子要素を中央に配置するスタイルを追加しました。これにより、ユーザーインターフェースが改善されました。

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,8 +2,10 @@ export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>)  {
+}>) {
   return (
-    <div>{children}</div>
-  )
+    <div className="min-h-screen flex items-center justify-center p-4">
+      {children}
+    </div>
+  );
 }
