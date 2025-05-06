ca56db4dbc62b5b841e0e4c2e4817a53fe5d3f8b
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-07 07:10:38 +0900

    認証機能を拡張し、Credentialsプロバイダーを追加しました。これにより、ユーザーの認証方法が増えました。

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
 
-export const { auth, signIn, signOut } = NextAuth({
+export const { auth, signIn, signOut, handlers } = NextAuth({
   ...authConfig,
+  providers: [Credentials({})],
 });
