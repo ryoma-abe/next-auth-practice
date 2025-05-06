3ee514a7f5aadf30fe129710fa75260ff5bc60d8
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-06 20:59:57 +0900

    Homeコンポーネントを非同期関数に変更し、getPosts関数を使用して投稿を取得し、PostCardコンポーネントで表示するようにしました。

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div></div>;
+import PostCard from "@/components/post/PostCard";
+import { getPosts } from "@/lib/post";
+
+export default async function Home() {
+  const posts = await getPosts();
+  return (
+    <>
+      <div className="container mx-auto">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post}/>
+        ))}
+      </div>
+    </>
+  );
 }
