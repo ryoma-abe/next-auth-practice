a69fed9653a2a5270258129936b0cb8799858656
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-07 07:06:24 +0900

    認証設定を更新し、ダッシュボードと管理ページへのアクセス制御を強化しました。未ログイン時にログインページからダッシュボードへのリダイレクトを追加しました。

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -7,11 +7,13 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      const isOnDashboard =
+        nextUrl.pathname.startsWith("/dashboard") ||
+        nextUrl.pathname.startsWith("/manage");
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false;
-      } else if (isLoggedIn) {
+      } else if (isLoggedIn && nextUrl.pathname === "/login") {
         return Response.redirect(new URL("/dashboard", nextUrl));
       }
       return true;
