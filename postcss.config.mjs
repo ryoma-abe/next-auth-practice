3d1a310be9c0899860a6a3b1174cbe6d249fc237
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-09 20:19:46 +0900

    postcss.config.mjsにtailwindcssAnimateと@tailwindcss/typographyを追加し、プラグインの設定を更新しました。これにより、スタイルの拡張性が向上します。

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,11 @@
+// postcss.config.mjs
+
+import tailwindcssAnimate from "tailwindcss-animate";
+import tailwindTypography from "@tailwindcss/typography";
+import tailwindcss from "@tailwindcss/postcss";
+
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: [tailwindcssAnimate, tailwindTypography, tailwindcss],
 };
 
 export default config;
