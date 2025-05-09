3ac6f28bbec92c016ce018aaa447ed7295e74b2d
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-09 17:20:58 +0900

    ダッシュボードページに認証機能を追加し、ユーザーIDの取得と不正リクエスト時のエラーハンドリングを実装しました。また、ユーザーの投稿を取得する機能を追加しました。

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import { auth } from "@/auth";
+import { getOwnPosts } from "@/lib/ownPost";
+
 export default async function DashboardPage() {
-  return (
-    <div>DashboardPage</div>
-  )
+  const session = await auth();
+  const userId = session?.user?.id;
+  if (!session?.user?.email || !userId) {
+    throw new Error("不正なリクエストです");
+  }
+  const posts = await getOwnPosts(userId);
+  return <div>DashboardPage</div>;
 }
