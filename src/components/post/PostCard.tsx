0192dc46b0b3f354069b56fe38547ee9fee10a05
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-10 16:51:12 +0900

    投稿カードコンポーネントにMarkdown表示機能を追加し、投稿内容をコードハイライト付きで表示するようにしました。これにより、投稿内容がより視覚的に表現されるようになります。

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -1,12 +1,26 @@
 import { PostCardProps } from "@/types/post";
 import Link from "next/link";
-
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github.css"; // コードハイライト用のスタイル
 export default function PostCard({ post }: PostCardProps) {
   return (
     <li className="border p-4">
       <Link href={`/post/${post.id}`}>
         <p>{post.author.name}</p>
-        <p>{post.content}</p>
+        <p>
+          <div className="border p-4 prose">
+            <ReactMarkdown
+              remarkPlugins={[remarkGfm]}
+              rehypePlugins={[rehypeHighlight]}
+              skipHtml={false}
+              unwrapDisallowed={true}
+            >
+              {post.content}
+            </ReactMarkdown>
+          </div>
+        </p>
       </Link>
     </li>
   );
