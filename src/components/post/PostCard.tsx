2d5f7fd72ee7f6a7ee8faf34b7b0addb0283a091
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-10 16:48:49 +0900

    投稿カードコンポーネントにリンク機能を追加し、投稿者名と内容をクリック可能にしました。これにより、ユーザーは投稿詳細ページに直接アクセスできるようになります。

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -1,10 +1,13 @@
 import { PostCardProps } from "@/types/post";
+import Link from "next/link";
 
 export default function PostCard({ post }: PostCardProps) {
   return (
     <li className="border p-4">
-      <p>{post.author.name}</p>
-      <p>{post.content}</p>
+      <Link href={`/post/${post.id}`}>
+        <p>{post.author.name}</p>
+        <p>{post.content}</p>
+      </Link>
     </li>
   );
 }
