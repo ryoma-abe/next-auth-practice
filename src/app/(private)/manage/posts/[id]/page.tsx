a9492113b852efaf4b490f8ee61f636c0caa19b2
ryoma-abe <liaozhenanbu387@gmail.com>
2025-05-10 15:47:46 +0900

    投稿詳細ページの表示を改善し、カードコンポーネントを使用して投稿情報を整理しました。投稿者名と作成日を表示し、トップ画像を含む新しいレイアウトを実装しました。

--- src/app/(private)/manage/posts/[id]/page.tsx
+++ src/app/(private)/manage/posts/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { auth } from "@/auth";
-import PostCard from "@/components/post/PostCard";
+import { format } from "date-fns";
+import { ja } from "date-fns/locale";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getOwnPost } from "@/lib/ownPost";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 type Params = {
@@ -18,5 +21,33 @@ export default async function ShowPage({ params }: Params) {
   if (!post) {
     notFound();
   }
-  return <PostCard key={post.id} post={post} />;
+  return (
+    <div>
+      <Card>
+        {post.topImage && (
+          <div className="relative">
+            <Image
+              src={post.topImage}
+              alt={post.title}
+              fill
+              sizes="100vw"
+              priority
+            />
+          </div>
+        )}
+        <CardHeader>
+          <div>
+            <p>投稿者：{post.author.name}</p>
+            <time>
+              {format(new Date(post.createdAt), "yyyy年MM月dd日", {
+                locale: ja,
+              })}
+            </time>
+          </div>
+        </CardHeader>
+        <CardTitle>{post.title}</CardTitle>
+        <CardContent>{post.content}</CardContent>
+      </Card>
+    </div>
+  );
 }
